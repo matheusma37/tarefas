4c001694d7abd7df595fd87a4a832c72eb276e9a
matheusma37 <mtsma37@gmail.com>
2019-06-26 14:48:12 -0300

    Cria o model pessoa e renomeia o projeto.

--- src/main/java/com/iff/webmobile/tarefas/TarefasApplication.java
+++ src/main/java/com/iff/webmobile/tarefas/TarefasApplication.java
@@ -1,13 +1,13 @@
-package com.iff.webmobile.chat;
+package com.iff.webmobile.tarefas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatApplication {
+public class TarefasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatApplication.class, args);
+		SpringApplication.run(TarefasApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/iff/webmobile/tarefas/models/Pessoa.java
@@ -0,0 +1,74 @@
+package com.iff.webmobile.tarefas.models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Pessoa implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToOne(mappedBy = "pessoa", optional = true)
+    private Tarefa tarefa;
+    
+    @NotNull(message = "Nome é obrigatório!")
+    @Column(length=100)
+    private String name;
+    
+    @NotNull(message = "Email é obrigatório!")
+    @Column(length=100)
+    private String email;
+    
+    @Column(length=100)
+    private String funcao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Tarefa getTarefa() {
+        return tarefa;
+    }
+
+    public void setTarefa(Tarefa tarefa) {
+        this.tarefa = tarefa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
