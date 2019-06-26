43cf7adba25d47b803ce505d28e535aac63dbc6e
matheusma37 <mtsma37@gmail.com>
2019-06-26 14:55:43 -0300

    Cria o model tarefa.

---
+++ src/main/java/com/iff/webmobile/tarefas/models/Tarefa.java
@@ -0,0 +1,89 @@
+package com.iff.webmobile.tarefas.models;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Tarefa implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToOne(mappedBy = "pessoa", optional = true)
+    private Pessoa pessoa;
+    
+    @ManyToOne
+    @JoinColumn(name = "grupo_id")
+    @NotNull(message = "Grupo é obrigatório!")
+    private Grupo grupo;
+    
+    @NotNull(message = "Descrição é obrigatória!")
+    @Column(length=500)
+    private String descricao;
+    
+    @NotNull(message = "Data de início é obrigatória!")
+    private Date dataInicio;
+    
+    @NotNull(message = "Data de fim é obrigatória!")
+    private Date dataFim;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+}
