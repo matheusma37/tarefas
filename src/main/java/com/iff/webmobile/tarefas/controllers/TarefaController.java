94a97a0b656f1fe7dc8934c85703aaefec9359a5
matheusma37 <mtsma37@gmail.com>
2019-06-27 13:09:28 -0300

    Implementa o controller do model tarefa.

--- src/main/java/com/iff/webmobile/tarefas/controllers/TarefaController.java
+++ src/main/java/com/iff/webmobile/tarefas/controllers/TarefaController.java
@@ -1,5 +1,81 @@
 package com.iff.webmobile.tarefas.controllers;
 
+import com.iff.webmobile.tarefas.models.Grupo;
+import com.iff.webmobile.tarefas.models.Tarefa;
+import com.iff.webmobile.tarefas.repositories.Pessoas;
+import com.iff.webmobile.tarefas.repositories.Grupos;
+import com.iff.webmobile.tarefas.repositories.Tarefas;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/grupo/{id_grupo}/tarefas")
 public class TarefaController {
+   
+    @Autowired
+    Pessoas pessoas;
     
+    @Autowired
+    Grupos grupos;
+    
+    @Autowired
+    Tarefas tarefas;
+
+    @RequestMapping("")
+    public ModelAndView listAll(@PathVariable Long id_grupo) {
+        ModelAndView mv = new ModelAndView("tarefa/tarefas-list");
+        mv.addObject("tarefa", new Tarefa());
+        Grupo grupo = grupos.getOne(id_grupo);
+        mv.addObject("pessoas", grupo.getPessoas());
+        mv.addObject("tarefas", grupo.getTarefas());
+        return mv;
+    }
+    
+    @RequestMapping(value="show/{id_tarefa}", method=RequestMethod.GET)
+    public ModelAndView show(@PathVariable Long id_grupo, @PathVariable Long id_tarefa) {
+        ModelAndView mv = new ModelAndView("tarefa/tarefa-show");
+        Grupo grupo = grupos.getOne(id_grupo);
+        Tarefa tarefa = null;
+        
+        for(Tarefa t: grupo.getTarefas()){
+            if(t.getId().equals(id_tarefa)){
+                tarefa = t;
+            }
+        }
+        
+        mv.addObject("tarefa", tarefa);
+        return mv;
+    }
+
+    @RequestMapping(value="",method=RequestMethod.POST)
+    public String save(Tarefa tarefa) {
+        tarefas.save(tarefa);
+        return "redirect:/grupo/"+tarefa.getGrupo().getId().toString()+"/tarefas";
+    }
+
+    @RequestMapping(value="update/{id_tarefa}")
+    public ModelAndView update(@PathVariable Long id_grupo, @PathVariable Long id_tarefa) {
+        ModelAndView mv = new ModelAndView("tarefa/tarefa-edit");
+        Grupo grupo = grupos.getOne(id_grupo);
+        Tarefa tarefa = null;
+        
+        for(Tarefa t: grupo.getTarefas()){
+            if(t.getId().equals(id_tarefa)){
+                tarefa = t;
+            }
+        }
+        
+        mv.addObject("tarefa", tarefa);
+        return mv;
+    }
+
+    @RequestMapping(value="/delete/{id_tarefa}")
+    public String delete(@PathVariable Long id_grupo, @PathVariable Long id_tarefa) {
+        tarefas.deleteById(id_tarefa);
+        return "redirect:/grupo/"+id_grupo.toString()+"/tarefas";
+    }
 }
