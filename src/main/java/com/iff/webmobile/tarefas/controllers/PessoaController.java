97f67af5036fb8f584dfa51103c4b4028701a826
matheusma37 <mtsma37@gmail.com>
2019-06-27 12:44:08 -0300

    Implementa o controller do model pessoa.

--- src/main/java/com/iff/webmobile/tarefas/controllers/PessoaController.java
+++ src/main/java/com/iff/webmobile/tarefas/controllers/PessoaController.java
@@ -1,5 +1,57 @@
 package com.iff.webmobile.tarefas.controllers;
 
+import com.iff.webmobile.tarefas.models.Pessoa;
+import com.iff.webmobile.tarefas.repositories.Grupos;
+import com.iff.webmobile.tarefas.repositories.Pessoas;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/pessoas")
 public class PessoaController {
+   
+    @Autowired
+    Pessoas pessoas;
     
+    @Autowired
+    Grupos grupos;
+
+    @RequestMapping("")
+    public ModelAndView listAll() {
+        ModelAndView mv = new ModelAndView("pessoa/pessoas-list");
+        mv.addObject("pessoa", new Pessoa());
+        mv.addObject("grupos",grupos.findAll());
+        mv.addObject("pessoas", pessoas.findAll());
+        return mv;
+    }
+    
+    @RequestMapping(value="show/{id}", method=RequestMethod.GET)
+    public ModelAndView show(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("pessoa/pessoa-show");
+        mv.addObject("pessoa", pessoas.getOne(id));
+        return mv;
+    }
+
+    @RequestMapping(value="",method=RequestMethod.POST)
+    public String save(Pessoa pessoa) {
+        pessoas.save(pessoa);
+        return "redirect:/pessoas";
+    }
+
+    @RequestMapping(value="update/{id}")
+    public ModelAndView update(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("pessoa/pessoa-edit");
+        mv.addObject("pessoa", pessoas.getOne(id));
+        return mv;
+    }
+
+    @RequestMapping(value="/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        pessoas.deleteById(id);
+        return "redirect:/pessoas";
+    }
 }
