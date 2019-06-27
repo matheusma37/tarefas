4a2662f96551bbf012957a3a782bc1273908e22c
matheusma37 <mtsma37@gmail.com>
2019-06-27 12:30:40 -0300

    Implementa o controller do model grupo.

--- src/main/java/com/iff/webmobile/tarefas/controllers/GrupoController.java
+++ src/main/java/com/iff/webmobile/tarefas/controllers/GrupoController.java
@@ -1,5 +1,52 @@
 package com.iff.webmobile.tarefas.controllers;
 
+import com.iff.webmobile.tarefas.models.Grupo;
+import com.iff.webmobile.tarefas.repositories.Grupos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/grupos")
 public class GrupoController {
+   
+    @Autowired
+    Grupos grupos;
+
+    @RequestMapping("")
+    public ModelAndView listAll() {
+        ModelAndView mv = new ModelAndView("grupo/grupos-list");
+        mv.addObject("grupo", new Grupo());
+        mv.addObject("grupos",grupos.findAll());
+        return mv;
+    }
     
+    @RequestMapping(value="show/{id}", method=RequestMethod.GET)
+    public ModelAndView show(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("grupo/grupo-show");
+        mv.addObject("grupo", grupos.getOne(id));
+        return mv;
+    }
+
+    @RequestMapping(value="",method=RequestMethod.POST)
+    public String save(Grupo grupo) {
+        grupos.save(grupo);
+        return "redirect:/grupos";
+    }
+
+    @RequestMapping(value="update/{id}")
+    public ModelAndView update(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("grupo/grupo-edit");
+        mv.addObject("grupo", grupos.getOne(id));
+        return mv;
+    }
+
+    @RequestMapping(value="/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        grupos.deleteById(id);
+        return "redirect:/grupos";
+    }
 }
